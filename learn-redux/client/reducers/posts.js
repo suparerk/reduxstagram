31b8faf755c325cb6f5dbcd77835f7e22980753e
suparerk <ch.suparerk@gmail.com>
2017-03-22 02:05:54 +0700

    finish learn redux course

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,23 @@
+//a reducer takes in two things
+
+//1. the action (informtion about what happend)
+//2. copy of current state
+
+function posts(state = [], action) {
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+    //return the update state
+      const i = action.index;
+      return[
+        ...state.slice(0,i), //before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), //after the one we are updating
+      ]
+    default:
+      return state;
+
+  }
+  return state;
+}
+
+export default posts;
